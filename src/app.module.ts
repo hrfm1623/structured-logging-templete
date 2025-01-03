31eb42aca18803c043bb9ba3316cbfbb5735c63b
hrfm1623 <hiro3510enic@gmail.com>
2025-01-03 09:25:17 +0900

    fix: deploy error

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule } from './common/logger/logger.module';
+import { LogLevel } from './common/logger/logger.interface';
 
 @Module({
   imports: [
@@ -12,7 +13,10 @@ import { LoggerModule } from './common/logger/logger.module';
       ignoreEnvFile: process.env.NODE_ENV === 'production',
       envFilePath: ['.env'],
     }),
-    LoggerModule,
+    LoggerModule.forRoot({
+      level: (process.env.LOG_LEVEL || 'info') as LogLevel,
+      useJson: process.env.USE_JSON_LOGGER === 'true',
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
